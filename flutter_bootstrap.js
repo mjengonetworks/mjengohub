473e55de5a431d7fe96450250a3f3e4cc7349676
mjengonetworks <mjengonetworks@users.noreply.github.com>
2026-09-21 10:47:29 +0000

    deploy: e111f32cd214b82ef8a31528757f02c5f212e5f9

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -42,6 +42,6 @@ _flutter.buildConfig = {"engineRevision":"af7e796e161ae0bb1ff0758c71a7105418bd9d
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2100466969" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3331491804" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
